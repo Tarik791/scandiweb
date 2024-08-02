7ccd976f2fdccee7db47da8234e51e591d2a66a5
Tarik791 <terzotarik@gmail.com>
2024-08-02 13:21:29 +0200

    Add Database configuration class with singleton pattern for PDO connection.

---
+++ src/Config/Config.php
@@ -0,0 +1,16 @@
+<?php
+// src/Config/Config.php
+
+return [
+    'localhost' => [
+        'dsn' => 'mysql:host=127.0.0.1;dbname=product_db',
+        'username' => 'root',
+        'password' => ''
+    ],
+    'production' => [
+        'dsn' => 'mysql:host=195.35.59.64;dbname=u123435024_product_db',
+        'username' => 'u123435024_product_root',
+        'password' => '6E>!^fDe0eyo'
+    ]
+];
+
